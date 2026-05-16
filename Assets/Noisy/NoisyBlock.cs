d56abd1cde8698b7513d51469c0b3877ef815cc2
Truncated Code <truncatedcode@gmail.com>
2026-05-17 00:56:46 +0300

    Add synced noisy block interactions

---
+++ Assets/Noisy/NoisyBlock.cs
@@ -0,0 +1,121 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+public class NoisyBlock : UdonSharpBehaviour
+{
+    public int blockType;
+    public NoisyTable noisyTable = null;
+    public bool attached = false;
+    public Rigidbody rb;
+    public MeshRenderer blockRenderer;
+    public Color touchingColor = Color.green;
+
+    private Color originalColor;
+    private bool hasOriginalColor;
+    private bool held;
+
+    private void Start()
+    {
+        StoreOriginalColor();
+        RefreshColor();
+    }
+
+    public MeshRenderer GetBlockRenderer()
+    {
+        if (blockRenderer == null)
+        {
+            blockRenderer = GetComponent<MeshRenderer>();
+        }
+
+        if (blockRenderer == null)
+        {
+            blockRenderer = GetComponentInChildren<MeshRenderer>();
+        }
+
+        return blockRenderer;
+    }
+
+    public void RefreshColor()
+    {
+        if (attached && held)
+        {
+            ApplyColor(touchingColor);
+            return;
+        }
+
+        ApplyOriginalColor();
+    }
+
+    public override void OnPickup()
+    {
+        held = true;
+        RefreshColor();
+    }
+
+    public override void OnDrop()
+    {
+        held = false;
+        RefreshColor();
+
+        Debug.Log("Dropped cube");
+        if (noisyTable!=null && attached==true) {
+            noisyTable.SnapBlock(this);
+            noisyTable.SetBlockPlaying(this, true);
+        }
+        if (noisyTable!=null && attached==false) {
+            noisyTable.SetBlockPlaying(this, false);
+            noisyTable = null;
+        }
+    }
+
+    private void StoreOriginalColor()
+    {
+        MeshRenderer renderer = GetBlockRenderer();
+        if (renderer == null)
+        {
+            return;
+        }
+
+        originalColor = renderer.material.color;
+        hasOriginalColor = true;
+    }
+
+    private void ApplyOriginalColor()
+    {
+        if (!hasOriginalColor)
+        {
+            StoreOriginalColor();
+        }
+
+        if (hasOriginalColor)
+        {
+            ApplyColor(originalColor);
+        }
+    }
+
+    private void ApplyColor(Color color)
+    {
+        MeshRenderer renderer = GetBlockRenderer();
+        if (renderer == null)
+        {
+            return;
+        }
+
+        renderer.material.color = color;
+    }
+
+    public int GetBlockTypeIndex()
+    {
+        if (blockType < 0)
+        {
+            return 0;
+        }
+
+        if (blockType > 2)
+        {
+            return 2;
+        }
+
+        return blockType;
+    }
+}
---
+++ Assets/Noisy/NoisyTable.cs
@@ -0,0 +1,112 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class NoisyTable : UdonSharpBehaviour
+{
+    public Transform snapPoint;
+    public AudioSource[] sources = new AudioSource[3];
+
+    [UdonSynced]
+    private int playingMask;
+
+    private void Start()
+    {
+        ApplyAudioVolumes();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        NoisyBlock b = other.GetComponent<NoisyBlock>();
+        if (b != null) {
+            b.noisyTable = this;
+            b.attached = true;
+            b.RefreshColor();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        NoisyBlock b = other.GetComponent<NoisyBlock>();
+        if (b != null && b.noisyTable == this) {
+            b.attached = false;
+            b.RefreshColor();
+            Debug.Log("OnTriggerExit (NoisyBlock)");
+        }
+    }
+
+    public override void OnDeserialization()
+    {
+        ApplyAudioVolumes();
+    }
+
+    public void SetBlockPlaying(NoisyBlock b, bool shouldPlay)
+    {
+        if (!Networking.IsOwner(gameObject))
+        {
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+
+        int blockType = b.GetBlockTypeIndex();
+        int blockMask = 1 << blockType;
+
+        if (shouldPlay)
+        {
+            playingMask |= blockMask;
+        }
+        else
+        {
+            playingMask &= ~blockMask;
+        }
+
+        ApplyAudioVolumes();
+        RequestSerialization();
+    }
+
+    public void SnapBlock(NoisyBlock b)
+    {
+        Vector3 position = b.transform.position;
+        Vector3 blockAngles = b.transform.eulerAngles;
+
+        if (snapPoint != null)
+        {
+            Vector3 snapAngles = snapPoint.eulerAngles;
+
+            position.y = snapPoint.position.y;
+            b.transform.position = position;
+            b.transform.rotation = Quaternion.Euler(snapAngles.x, blockAngles.y, snapAngles.z);
+        }
+        else
+        {
+            Vector3 tableAngles = transform.eulerAngles;
+            b.transform.rotation = Quaternion.Euler(tableAngles.x, blockAngles.y, tableAngles.z);
+        }
+
+        if (b.rb != null)
+        {
+            b.rb.velocity = Vector3.zero;
+            b.rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    private void ApplyAudioVolumes()
+    {
+        if (sources == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < sources.Length; i++)
+        {
+            AudioSource audioSource = sources[i];
+            if (audioSource == null)
+            {
+                continue;
+            }
+
+            audioSource.volume = (playingMask & (1 << i)) != 0 ? 1f : 0f;
+        }
+    }
+
+}
